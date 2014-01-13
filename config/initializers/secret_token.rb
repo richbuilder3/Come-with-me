96db0e6691457f543b3b695337b5b181d2cb5111
Richbuilder3 <richard.c.grundy@gmail.com>
2014-01-13 14:06:13 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComeWithMe::Application.config.secret_token = 'b266219cd0e0541bd1d317850e3f77179f6426a8c69b02b4a47faee9d2bbd1f8e0c67c971835f0a63d25bab04b5f84534589857a12c98b3ca61eda53a1608074'
